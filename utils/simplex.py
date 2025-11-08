9dba174e8c71ef2c33f043510af491c974ca9741
Arvin Ferrer <60701881+arvin-ferrer@users.noreply.github.com>
2025-11-08 13:41:16 +0800

    Added simplex function

--- utils/simplex.py
+++ utils/simplex.py
@@ -1,14 +1,27 @@
+import numpy as numpy
+import pandas as pd
 
-def solve_simplex(initial_tableau):
-    tableau_history = [] 
+def simplex(tableau, is_max=True):
+    tableau = tableau.astype(float)
+    n, m = tableau.shape
     
-    current_tableau = initial_tableau
-    tableau_history.append(current_tableau)
+    while np.min(tableau[-1, :-1]) < 0:
+        pivot_col = np.argmin(tableau[-1, :-1])
+        ratios = np.array([
+            tableau[i, -1] / tableau[i, pivot_col] if tableau[i, pivot_col] > 0 else np.inf
+            for i in range(n - 1)
+        ])
+        pivot_row = np.argmin(ratios)
+        tableau[pivot_row, :] /= tableau[pivot_row, pivot_col]
+        for i in range(n):
+            if i != pivot_row:
+                tableau[i, :] -= tableau[i, pivot_col] * tableau[pivot_row, :]
     
-    while not is_optimal(current_tableau):
-        current_tableau = pivot(current_tableau)
-        tableau_history.append(current_tableau)
-        
-    final_solution = parse_solution(current_tableau)
-    #test
-    return final_solution, tableau_history
+    # Extract basic variables
+    solution = np.zeros(m - 1)
+    for i in range(m - 1):
+        col = tableau[:, i]
+        if (col == 1).sum() == 1 and (col == 0).sum() == n - 1:
+            solution[i] = tableau[np.where(col == 1)[0][0], -1]
+    
+    return solution, tableau[-1, -1], tableau
