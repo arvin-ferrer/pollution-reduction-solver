2ca8105ee45f2cdc32befc8510024643ea230f01
Arvin Ferrer <60701881+arvin-ferrer@users.noreply.github.com>
2025-11-30 15:13:26 +0800

    fix: more simple setup

--- utils/createTableau.py
+++ utils/createTableau.py
@@ -1,49 +1,45 @@
-from utils.simplex import SimplexSolver
-
 import numpy as np
 import pandas as pd
 
 def createTableau(costVectorC, pollutantMatrixApoll, targetVectorBpoll):
-    M = 1e9
+    # get the number of projects 
     numProjects = len(costVectorC)
+    # get the number of pollutants
     numPollutants = len(targetVectorBpoll)
-    numConstraints = numPollutants + numProjects
-
-    numVars = numProjects
-    numSurplus = numPollutants
-    numSlack = numProjects
-    numArtificial = numPollutants
-
-    numCols = numVars + numSurplus + numSlack + numArtificial + 2
-    numRows = numConstraints + 1
-    tableau = np.zeros((numRows, numCols))
-
-    # pollutant constraints
-    tableau[:numPollutants, :numProjects] = pollutantMatrixApoll
-    for i in range(numPollutants):
-        tableau[i, numProjects + i] = -1.0          
-        tableau[i, numProjects + numSurplus + numSlack + i] = 1.0  
-    tableau[:numPollutants, -1] = targetVectorBpoll
-
-    # constraints
-    for i in range(numProjects):
-        rowIdx = numPollutants + i
-        tableau[rowIdx, i] = 1.0
-        tableau[rowIdx, numProjects + numSurplus + i] = 1.0 # slack
-    tableau[numPollutants:numConstraints, -1] = 20.0 
-
-
-    zRowIdx = numConstraints
+    # pollutant coeff matrix
+    A_poll = pollutantMatrixApoll
+    # the pollutant vector to target
+    b_poll = targetVectorBpoll.reshape(-1, 1)
+    # project limits x >= 20 transformed to -x <= -20
+    A_limits = np.eye(numProjects) * -1
+    # limit value 20 projects
+    b_limits = np.full((numProjects, 1), -20.0)
+    # stacking the pollutant matrix and the limits
+    A_total = np.vstack([A_poll, A_limits])
+    # same with A_total
+    b_total = np.vstack([b_poll, b_limits])
+     # objective function (costs)
+    c_total = costVectorC.reshape(1, -1)
+    # transposing the matrix
+    A_dual = A_total.T
+    b_dual = c_total.T
+    c_dual = -b_total.T
     
-    tableau[zRowIdx, :numProjects] = costVectorC
+    # adding the slack variables
+    numDualVars = A_dual.shape[1] # 23 (pollutants + limits (constraints))
+    numSlacks = A_dual.shape[0]   # 13 (one per project)
     
-    for i in range(numArtificial):
-        colIdx = numVars + numSurplus + numSlack + i
-        tableau[zRowIdx, colIdx] = M
-        
-    tableau[zRowIdx, -2] = 1.0 # the Z' column
-
-    for i in range(numPollutants):
-        tableau[zRowIdx, :] -= M * tableau[i, :]
-    # print(tableau)
+    rows = numSlacks + 1
+    cols = numDualVars + numSlacks + 2
+    tableau = np.zeros((rows, cols))
+    # fill coefficients
+    tableau[:numSlacks, :numDualVars] = A_dual
+    tableau[:numSlacks, numDualVars : numDualVars + numSlacks] = np.eye(numSlacks)
+    # fill rhs
+    tableau[:numSlacks, -1] = b_dual.flatten()
+    # fill objective row
+    tableau[-1, :numDualVars] = c_dual
+    
+    # fill Z column
+    tableau[-1, -2] = 1.0
     return tableau
